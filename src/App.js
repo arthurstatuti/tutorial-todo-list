3641ee8178533741471e9bf9bbafc416e063180e
Luiz Casimiro <luizcasimi@gmail.com>
2022-02-17 10:04:14 -0300

    set up Form and TodoList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+/* eslint-disable no-console */
+
+// App based on Simo Edwin's tutorial:
+// https://www.youtube.com/watch?v=pCA4qpQDZD8
+
 import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>I am the App</h1>
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+
+function TodoList() {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+  );
+}
+
+export default TodoList;
